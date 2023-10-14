129ac1249c723d525c62b524a80659845b6bded8
Nolan Perry <nolantheperry@gmail.com>
2023-10-14 15:50:15 -0400

    Create GoogleFormsToDiscord.js

---
+++ GoogleFormsToDiscord.js
@@ -0,0 +1,59 @@
+const POST_URL = "";
+
+function onSubmit(e) {
+    const response = e.response.getItemResponses();
+    let items = [];
+
+    for (const responseAnswer of response) {
+        const question = responseAnswer.getItem().getTitle();
+        const answer = responseAnswer.getResponse();
+        let parts = []
+
+        try {
+            parts = answer.match(/[\s\S]{1,1024}/g) || [];
+        } catch (e) {
+            parts = answer;
+        }
+
+        if (!answer) {
+            continue;
+        }
+
+        for (const [index, part] of Object.entries(parts)) {
+            if (index == 0) {
+                items.push({
+                    "name": question,
+                    "value": part,
+                    "inline": false
+                });
+            } else {
+                items.push({
+                    "name": question.concat(" (cont.)"),
+                    "value": part,
+                    "inline": false
+                });
+            }
+        }
+    }
+
+    const options = {
+        "method": "post",
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "payload": JSON.stringify({
+            "content": "‌",
+            "embeds": [{
+                "title": "Fort Freyview Job Applications",
+                "color": 33023, // This is optional, you can look for decimal colour codes at https://www.webtoolkitonline.com/hexadecimal-decimal-color-converter.html
+                "fields": items,
+                "footer": {
+                    "text": "Footer? I hardly knew her."
+                },
+                "timestamp": new Date().toISOString()
+            }]
+        })
+    };
+
+    UrlFetchApp.fetch(POST_URL, options);
+};
